26001c9f120bf22916f3ab1778a93e3cc1c5ab35
RomarioLopCa <romario_lopezc@hotmail.com>
2015-09-01 19:06:57 -0500

    Index de alumno y tutor.

--- Primer Parcial/Tareas/Tarea1/FrontEnd/menu.php
+++ Primer Parcial/Tareas/Tarea1/FrontEnd/menu.php
@@ -9,3 +9,25 @@ session_start();
 if (!isset($_SESSION["username"])) {
     header('Location: login.html');
 }
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>AJAX Tarea 1</title>
+    </head>
+    <body>
+        <div style="text-align: center">
+            <table>
+                <tr>
+                    <th colspan="2">Menú</th>
+                </tr>
+                <tr>
+                    <td><a href="alumno/index.html">Alumnos</a></td>
+                    <td><a href="tutor/index.html">Tutores</a></td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
